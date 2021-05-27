cfae03f52d19401c90d66d9e0b44d242e554b3f1
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-05-27 11:52:53 +0530

    Adding function files

---
+++ function/palindromeCheck.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+read -p "enter the number to check palindrome: " number
+
+originalNumber=$number
+function palindromeCheck() {
+	while [ $number -ne 0 ]
+	do
+		rem=$(($number%10))
+		rev=$((rev*10+rem))
+		number=$(($number/10))
+	done
+	echo $rev
+	if [ $originalNumber -eq $rev ]
+	then
+		echo "number is palindrme"
+	else
+		echo "number is not palindrome"
+	fi
+}
+echo result="$(palindromeCheck $number)"
